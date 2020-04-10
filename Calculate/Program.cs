786c8e3c770a2c6d3158fe2965211368fe337403
Sonellon <52424969+Sonellon@users.noreply.github.com>
2020-04-10 13:36:30 +0500

    Add files via upload

---
+++ Calculate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Write("Input expression: ");
+                double output=Сhanged.Calculate(Console.ReadLine());
+               
+                    Console.WriteLine(output);
+               
+            }
+        }
+    }
+}
